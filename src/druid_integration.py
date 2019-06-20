4fd18d1a08021f3c326e2f6dc531857b12a9b154
Roderick van der Weerdt <rvanderweerdt@hotmail.com>
2019-06-20 09:58:17 +0200

    Revert "moved druid integration from cattle_main"
    This reverts commit e6d4b3c518dc25c69e51c768e4244fa0ab2b57d4.

---
+++ src/druid_integration.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+
+# cattle: A Web service for COW
+
+# from flask import Flask, request, render_template, make_response, redirect, jsonify
+from werkzeug.utils import secure_filename
+# import requests
+import os
+import subprocess
+import json
+from rdflib import ConjunctiveGraph
+from cow_csvw.csvw_tool import COW
+# import StringIO
+import gzip
+# import shutil
+# import traceback
+from hashlib import md5
+from time import time
+from mail_templates import send_new_graph_message
+import codecs
+
+import info_log
+
+#create hash to use as folder to save the files
+def create_hash(csv_file, json_file, read_files=True, logger=None):
+	m = md5()
+	if read_files:
+		if json_file == None:
+			m.update(csv_file.read())
+			csv_file.seek(0)
+		else:
+			m.update(csv_file.read() + json_file.read())
+			csv_file.seek(0)
+			json_file.seek(0)
+	else:
+		n_intervals = 5.0 #if some illegal character disrupts the creation of the hash, a subset of the file is tried
+		i = n_intervals
+		while(i > 0):
+			try:
+				# print("creating a hash with {}% of the files..".format(100*(i/n_intervals)))
+				m.update(csv_file[:int(round(len(csv_file)*(i/n_intervals)))]+json_file[:int(round(len(json_file)*(i/n_intervals)))])
+				# print("succesfully created a hash with {}% of the files.".format(100*(i/n_intervals)))
+				break
+			except:
+				i -= 1
+		if logger != None:
+			logger.debug("succesfully created a hash with {}% of the files.".format(100*(i/n_intervals)))
+	return m.hexdigest()
+
+def make_hash_folder(path, csv_file, json_file=None):
+	hash_folder_name = create_hash(csv_file, json_file)
+	new_path = os.path.join(path, "web_interface", hash_folder_name)
+	try:
+		os.makedirs(new_path)
+	except:
+		# self.logger.debug("This folder already exists, this might result in concurrency problems.")
+		pass
+	return new_path
+
+def make_hash_folder_druid(csv_string, username, dataset, path, json_string="", logger=None):
+	hash_folder_name = create_hash(csv_string, json_string, False, logger)
+	try:
+		os.makedirs(os.path.join(path, username, dataset, hash_folder_name))
+	except:
+		# self.logger.debug("This folder already exists, this might result in concurrency problems.")
+		pass
+	return os.path.join(username, dataset, hash_folder_name)
+
+def make_hash_path_druid(csv_string, username, dataset, json_string=""):
+	hash_folder_name = create_hash(csv_string, json_string, False)
+	return os.path.join(username, dataset, hash_folder_name)
+
+
+### Druid interface
+class druid2cattle:
+	def __init__(self, username, dataset, logger, upload_folder, requests, auth_token):
+		self.username = username
+		self.dataset = dataset
+		self.logger = logger
+		self.upload_folder = upload_folder
+		self.requests = requests
+		self.token = auth_token
+
+		#changes every time
+		self.path = ""
+
+	#retrieves all the .csv-files and .csv- .json-file pairs from a specific druid dataset.
+	def get_candidates(self):
+		self.logger.debug("Listing remote files in Druid for user {} dataset {}".format(self.username, self.dataset))
+		r = self.requests.get("https://api.druid.datalegend.net/datasets/{}/{}/assets".format(self.username, self.dataset))
+		self.logger.debug("Remote files are: {}".format(r.json()))
+		files = json.loads(r.text)
+		url_dict = {}
+		for f in files: url_dict[f['assetName']] = f['url']
+		duos = {}
+		single_csv = {}
+		for f in url_dict.keys():
+			if f.endswith('.csv') and (f + '-metadata.json') in url_dict.keys():
+				duos[f] = (url_dict[f], url_dict[f + '-metadata.json'])
+			elif f.endswith('.csv'):
+				single_csv[f] = url_dict[f]
+
+		return duos, single_csv
+
+	#only return the candidate or single that corresponds with the basename of the file that
+	#triggered the webhook.
+	def select_candidate(self, candidates, singles, basename):
+		self.logger.debug("received a hook for the file: {}".format(basename))
+
+		new_candidates = {}
+		for found_asset in candidates.keys():
+			if found_asset == basename:
+				new_candidates[found_asset] = candidates[found_asset]
+
+		new_singles = {}
+		for found_asset in singles.keys():
+			if found_asset == basename:
+				new_singles[found_asset] = singles[found_asset]
+
+		candidates = new_candidates
+		singles = new_singles
+		return candidates, singles
+
+
+	#returns True if the hash folder already exists and has been modified in the last 60 seconds.
+	def check_for_concurrency(self):
+		csv_path = os.path.join(self.upload_folder, self.path)
+		if os.path.exists(csv_path):
+			if (time() - os.path.getmtime(csv_path) < 60): #hardcoded at 60 seconds.
+				self.logger.debug("The hash folder has been modified in the last " + str(time() - os.path.getmtime(csv_path)) + " seconds, so we will wait.")
+				return True
+			else:
+				self.logger.debug("\nNope, no concurrency problem I can smell.\n"+str(time() - os.path.getmtime(csv_path) < 60)+"\n")
+				return False
+		else:
+			self.logger.debug("\nNope, no folder exists, so no problem.")
+			return False
+
+	#downloads the csv json file pair from druid, returns False if the hash folder has been modified in the last 60 seconds,
+	#returns True otherwise.
+	def download_pair(self, f, pair):
+		csv_string = self.requests.get(pair[0]).content
+		json_string = self.requests.get(pair[1]).content
+		self.path = make_hash_path_druid(csv_string, self.username, self.dataset, json_string)
+
+		if self.check_for_concurrency():
+			return False
+
+		make_hash_folder_druid(csv_string, self.username, self.dataset, self.upload_folder, json_string, self.logger)
+
+		self.path = os.path.join(self.path, secure_filename(f))
+
+		with open(os.path.join(self.upload_folder, self.path), 'wb') as file_csv:
+			file_csv.write(csv_string)
+		with open(os.path.join(self.upload_folder, self.path + '-metadata.json'), 'wb') as file_json:
+			file_json.write(json_string)
+
+		self.logger.debug("File {} uploaded successfully".format(os.path.join(self.upload_folder, self.path)))
+		self.logger.debug("File {} uploaded successfully".format(os.path.join(self.upload_folder, self.path + '-metadata.json')))
+		return True
+
+	def handle_pairs(self, candidates):
+		# if .csv and .json pairs are present int the assets, downloads them, converts them, uploads results
+		self.logger.debug('Downloading and converting: {}'.format(candidates.keys()))
+		for f in candidates.keys():
+
+
+			if not self.download_pair(f, candidates[f]):
+				continue
+			infolog.job_end("download pair")
+			infolog = info_log.info_log(os.path.join(self.upload_folder, self.path))
+
+			infolog.job_start("druid_longer")
+			sub_log_name = infolog.sub_start("druid_longer")
+			try:
+				email_address = self.requests.json['user']['email']
+				account_name = self.requests.json['user']['accountName']
+			except:
+				email_address = ""
+				account_name = ""
+			with open(os.path.join(self.upload_folder, sub_log_name), 'w') as sub_log:
+				subprocess.Popen(["python", "src/druid_longer.py", "-path", os.path.join(self.upload_folder, self.path), "-token", self.token, "-dataset", self.dataset, "-username", self.username, "-email_address", email_address, "account_name", account_name], stdout=sub_log, stderr=sub_log)
+			infolog.job_end("druid_longer")
+			
+			# Convert & upload happens in subprocess(druid_longer.py)
+
+	def download_single(self, f, candidate):
+		# Downloads the csv
+		csv_string = self.requests.get(candidate).content
+		self.path = os.path.join(make_hash_folder_druid(csv_string, self.username, self.dataset, self.upload_folder, "", self.logger), secure_filename(f))
+
+		with open(os.path.join(self.upload_folder, self.path), 'wb') as file_csv:
+			file_csv.write(csv_string)
+
+		self.logger.debug("File {} uploaded successfully".format(os.path.join(self.upload_folder, self.path)))
+
+	# creates a json file for a csv using COW
+	def build_auto_json(self):
+		self.logger.debug("Running COW build")
+		COW(mode='build', files=[os.path.join(self.upload_folder, self.path)])
+		self.logger.debug("Build finished")
+
+	# Check if the json counterpart to a csv has been uploaded to the assets
+	def found_new_json(self, current_csv):
+		self.logger.debug("Listing remote files in Druid for user {} dataset {}".format(self.username, self.dataset))
+		r = self.requests.get("https://api.druid.datalegend.net/datasets/{}/{}/assets".format(self.username, self.dataset))
+		files = json.loads(r.text)
+		url_dict = {}
+		for f in files: url_dict[f['assetName']] = f['url']
+		if (current_csv + '-metadata.json') in url_dict.keys():
+			return True
+		else:
+			return False
+
+	# if .csv without json counterpart are present in the assets, downloads the csv, creates json, converts them, uploads results
+	# during the process checks will determine whether the json counterpart still isn't uploaded, if it is found the function will skip
+	# that csv json pair
+	def handle_singles(self, candidates):
+		self.logger.debug("Waiting for possible json-files.")
+		self.logger.debug("Starting with the single csv-files.")
+		for f in candidates.keys():
+			self.logger.debug("handling single: " + f)
+			self.download_single(f, candidates[f])
+			infolog = info_log.info_log(os.path.join(self.upload_folder, self.path))
+			infolog.job_end("download single")
+			if self.found_new_json(f):
+				self.logger.debug("Stopped because a new json was found.")
+				continue
+			self.build_auto_json()
+			if self.found_new_json(f):
+				self.logger.debug("Stopped because a new json was found.")
+				continue
+
+			sub_log_name = infolog.sub_start("druid_longer")
+			try:
+				email_address = self.requests.json['user']['email']
+				account_name = self.requests.json['user']['accountName']
+			except:
+				email_address = ""
+				account_name = ""
+				self.logger.debug("no email email_address was found..")
+			with open(os.path.join(self.upload_folder, os.path.dirname(self.path), sub_log_name), 'w') as sub_log:
+				subprocess.Popen(["python", "src/druid_longer.py", "-path", os.path.join(self.upload_folder, self.path), "-token", self.token, "-dataset", self.dataset, "-username", self.username, "-email_address", email_address, "-account_name", account_name], stdout=sub_log, stderr=sub_log)
+
+			# Convert & upload happens in subprocess(druid_longer.py)
+
